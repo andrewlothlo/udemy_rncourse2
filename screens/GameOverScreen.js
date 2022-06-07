987292751787883107fe92aa51fe7374115ffd43
andrew <andrewlothlo@gmail.com>
2022-06-07 16:17:27 +0800

    section 5 87  completed

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,13 +1,31 @@
-import { Text, View, Image, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, Image, StyleSheet, useWindowDimensions, ScrollView } from 'react-native';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
 import Colors from '../constants/color';
 
 function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
+const {width, height} = useWindowDimensions();
+
+let imageSize = 300;
+
+if (width < 380){
+    imageSize = 150;
+}
+
+if (height < 400) {
+    imageSize = 80;
+}
+
+const imageStyle = {
+    width: imageSize,
+    height: imageSize,
+    borderRadius: imageSize / 2
+}
     return (
+        <ScrollView style = {styles.screen}>
         <View style={styles.rootContainer}>
             <Title>Game Over!</Title>
-            <View style={styles.imageContainer}>
+            <View style={[styles.imageContainer, imageStyle]}>
                 <Image
                     source={require("../assets/success.png")}
                     style={styles.image}>
@@ -19,12 +37,13 @@ function GameOverScreen({roundsNumber, userNumber, onStartNewGame}) {
             </Text>
             <PrimaryButton onPress={onStartNewGame}>Start New Game</PrimaryButton>
         </View >
+        </ScrollView>
     );
 }
 
 export default GameOverScreen;
 
-const deviceWidth = Dimensions.get('window').width;
+//const deviceWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
     rootContainer: {
@@ -34,9 +53,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     imageContainer: {
-        borderRadius: deviceWidth < 380? 75: 150,
-        height: deviceWidth < 380? 150: 300,
-        width: deviceWidth < 380? 150: 300,
+        //borderRadius: deviceWidth < 380? 75: 150,
+        //height: deviceWidth < 380? 150: 300,
+        //width: deviceWidth < 380? 150: 300,
         borderWidth: 3,
         borderColor: Colors.primary800,
         overflow: 'hidden',
@@ -58,5 +77,7 @@ const styles = StyleSheet.create({
         color: Colors.primary500,
 
     },
-
+    screen: {
+        flex : 1,
+    },
 })
